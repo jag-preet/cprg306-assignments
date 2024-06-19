ab64c4f7ddf9b801ceac1fe8dac86649d2bcc06a
jag-preet <jagpreetsingh.2303@gmail.com>
2024-06-19 15:45:00 -0600

    updating assignments

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+      <main>
+        <h1>Jagpreet</h1>
+        <Link href="https://github.com/jag-preet">This is link to my Github account</Link>
+      </main>
+    );
+  }
